933fb2eb7a568f33e823a2affc4f16f978a7e5c0
Priya Dholakiya <priyadholakiya16@gmail.com>
2026-02-26 11:23:20 +0530

    NodeJS

--- PR-6 Blog/config/dbConnect.js
+++ PR-6 Blog/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = () => {
     mongoose.connect(
-        "mongodb+srv://shreyapal0411_db_user:shr%401234@practicecluster.vnl1eb0.mongodb.net/Blog"
+        "mongodb+srv://piyudholakiya81_db_user:paz2kRUp6Eyd5AiJ@cluster0.zjjtcyq.mongodb.net/?appName=Cluster0"
     )
     .then(() => console.log("Database connected successfully"))
     .catch((err) => console.log(err));
